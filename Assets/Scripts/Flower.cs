611841912620212b5f154da4acd244dc0a728fcd
epenning <epennington10@gmail.com>
2017-09-27 23:59:26 -0500

    prevent flowers from spawning too close to each other

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -17,17 +17,20 @@ public class Flower : MonoBehaviour {
     static readonly string SEEDHEAD_GAMEOBJECT = "flower1_seed_head";
 
     public enum Stage { Sprout, Flower, Pollinated, SeedHead, Dead };
-    public static readonly float sproutTime = 60;
+    public static readonly float sproutTime = 10; //60;
     public static readonly float flowerTime = 1000;
-    public static readonly float pollinatedTime = 10;
+    public static readonly float pollinatedTime = 5; //10;
     public static readonly float seedHeadTime = 1000;
     public static readonly int numberOfNewSprouts = 4;
     public static readonly float newSproutRadius = 60;
+    public static readonly float flowerRadius = 6;
 
     private Animator animator;
 
     public Stage stage;
     public float stageTimer;
+
+    List<Vector3> seedPositions = new List<Vector3>();
 
     private void Start() {
         sproutPrefab = Resources.Load(sproutPrefabPath) as GameObject;
@@ -123,21 +126,61 @@ public class Flower : MonoBehaviour {
         }
 
         SetStage(Stage.Dead);
+        Invoke("Die", 3);
     }
 
     public void SpawnSeed() {
-        float angle = Random.Range(0, 360);
-        float distance = Random.Range(0, newSproutRadius);
+        try {
+            Vector3 position = FindValidSpawnPosition();
+            seedPositions.Add(position);
 
-        float relativeX = distance * Mathf.Cos(angle);
-        float relativeY = distance * Mathf.Sin(angle);
+            float rotationAngle = Random.Range(0, 360);
+            Quaternion rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
 
-        float rotationAngle = Random.Range(0, 360);
+            Instantiate(sproutPrefab, position, rotation, sproutParent);
+        } catch (UnityException) {
+            // do not spawn flower if there is no space for it
+        }
+    }
 
-        Vector3 position = new Vector3(transform.position.x + relativeX, transform.position.y + relativeY, transform.position.z);
-        Quaternion rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+    public Vector3 FindValidSpawnPosition() {
+        int tries = 3;
+        Vector3 position = transform.position;
 
-        Instantiate(sproutPrefab, position, rotation, sproutParent);
+        for (int i = 0; i < tries; i++) {
+            float angle = Random.Range(0, 360);
+            float distance = Random.Range(0, newSproutRadius);
+
+            float relativeX = distance * Mathf.Cos(angle);
+            float relativeY = distance * Mathf.Sin(angle);
+
+            position = new Vector3(transform.position.x + relativeX, transform.position.y + relativeY, transform.position.z);
+
+            if (IsValidSpawnPosition(position)) {
+                return position;
+            }
+        }
+
+        throw new UnityException();
+    }
+    
+    public bool IsValidSpawnPosition(Vector3 position) {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll((Vector2)position, flowerRadius);
+        Debug.Log(hitColliders);
+
+        foreach (Collider2D hitCollider in hitColliders) {
+            if (hitCollider.CompareTag("Flower")) {
+                return false;
+            }
+        }
+
+        foreach (Vector3 seedPosition in seedPositions) {
+            if (Vector3.Distance(position, seedPosition) < flowerRadius) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public void Die() {
